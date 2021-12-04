9f3cd79e6ffd6d3cd786f2650f3462edfa215a28
yamanidev <elamine.yamani@gmail.com>
2021-12-04 08:44:32 +0100

    Copied notification added

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 const copyMockedBtn = document.getElementById("copy-mocked-btn");
 const toMockInput = document.getElementById("to-mock-input");
 const mockedOutput = document.getElementById("mocked-output");
+const copiedNotif = document.getElementById("copied-notif");
+const closeNotifBtn = document.getElementById("close-notif");
 
 function mock(str) {
     str = str.trim();
@@ -21,6 +23,12 @@ function mock(str) {
     return x;
 }
 
+function removeNotif(el) {
+    if (el.classList.contains("active")) {
+        el.classList.remove("active");
+    }
+}
+
 toMockInput.addEventListener("input", (e) => {
     mockedOutput.innerText = mock(e.target.value);
 });
@@ -30,6 +38,16 @@ copyMockedBtn.addEventListener("click", () => {
         alert("Empty field");
     } else {
         navigator.clipboard.writeText(mockedOutput.value);
-        alert("Mocked text copied!");
+        copiedNotif.classList.add("active");
+        setTimeout(
+            () => {
+                removeNotif(copiedNotif);
+            },
+            4000
+        );
     }
 });
+
+closeNotifBtn.addEventListener("click", () => {
+    removeNotif(copiedNotif);
+})
